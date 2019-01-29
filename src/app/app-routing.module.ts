afa643046410b8011f814f51feba71d811784f3f
Adrush Khatri <adrushkhatri@gmail.com>
2019-01-29 21:31:07 +0500

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
+  { path: 'movies/:id', loadChildren: './pages/movies-details/movies-details.module#MoviesDetailsPageModule' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
